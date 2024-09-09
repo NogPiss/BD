e5a90cfaca68091b4eb865d596be7bb022881209
Eduardo Pereira de Queiroz <eduardopq4@gmail.com>
2024-09-09 01:21:08 -0300

    Exercise 1 and 2 from the Functions/Procedures work

--- Exercícios_Functions_Procedures/Functions_Procedures.sql
+++ Exercícios_Functions_Procedures/Functions_Procedures.sql
@@ -1,6 +1,5 @@
-create database SistemasVendas
-use SistemasVendas
-
+create database TrabalhoBD
+use TrabalhoBD
 
 CREATE TABLE Clientes (
     clienteid INT NOT NULL,
@@ -15,7 +14,7 @@ CREATE TABLE Pedidos (
     cliente_clienteid INT NOT NULL,
     datapedido DATE,
     valortotal NUMERIC(12, 2),
-    descaplicado VARCHAR(150),
+    descaplicado numeric(12,2),
     PRIMARY KEY (pedidoid),
     FOREIGN KEY (cliente_clienteid) REFERENCES Clientes(clienteid)
 );
@@ -38,7 +37,6 @@ CREATE TABLE ItensPedidos (
     FOREIGN KEY (pedidos_pedidoid) REFERENCES Pedidos(pedidoid),
     FOREIGN KEY (produtos_produtoid) REFERENCES Produtos(produtoid)
 );
-
 
 -- Inserir dados na tabela Clientes
 INSERT INTO Clientes (clienteid, nome_cliente, cidade, datacad)
@@ -69,8 +67,65 @@ VALUES
 (3, 1002, 11, 5, 200.00),
 (4, 1003, 12, 1, 150.00);
 
+go
 
-select * from ItensPedidos
+--Exercício 1 - Consulta Clientes
+
+alter procedure ConsultaClientes
+as
+BEGIN
+	select c.nome_cliente, c.cidade, 
+	sum(p.valortotal) as TotalGasto,
+	(SELECT TOP 1 pr.nome_produto
+	from Produtos pr
+	join ItensPedidos ipe on pr.produtoid = ipe.produtos_produtoid
+	order by ipe.preco_unitario * ipe.quantidade DESC) as ProdutoMaisCaro
+	from Clientes c
+	inner join Pedidos p on c.clienteid = p.cliente_clienteid
+	where valortotal > 5000 and DATEDIFF(MONTH, datapedido, GETDATE()) <= 3
+	group by c.nome_cliente, c.cidade;
+END;
+
+go
+
+EXEC ConsultaClientes
+
+--Exercício 2
+
+go 
+
+go
+
+create procedure AplicaDesconto
+as
+Begin
+
+	begin transaction
+
+	Update Pedidos
+	set descaplicado = CASE
+		When TotalItens > 30 and pedidos.valortotal <= (TotalItens * 10) Then 0.15
+		When TotalItens > 20 and pedidos.valortotal <= (TotalItens * 10) Then 0.10
+		When TotalItens > 10 and pedidos.valortotal <= (TotalItens * 10) Then 0.05
+	ELSE 0.00
+	END
+	FROM(
+		SELECT p.pedidoid, count(i.itemid) as TotalItens, p.valortotal from Pedidos p
+		inner join ItensPedidos i on p.pedidoid = i.pedidos_pedidoid
+		where datapedido >= DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE()), 0)
+		and datapedido < DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE()) +1, 0)
+		GROUP BY p.pedidoid, p.valortotal
+	) AS SubQuery
+	where Pedidos.pedidoid = SubQuery.pedidoid
+
+	commit transaction
+end;
+
+go
+
+exec AplicaDesconto
+
+-- Exercício 3
 
 go 
 
@@ -86,3 +141,5 @@ begin
 end;
 
 exec interacao_produtos
+
+go
